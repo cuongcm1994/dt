2db6a58eff7159a0c7ef6308d313e9123735a28e
cuongcm1994 <abcdcuong@gmail.com>
2016-08-22 16:53:27 +0700

    upload all file by cuongnv

---
+++ catproduct/reeditcatproduct.php
@@ -0,0 +1,46 @@
+<?php
+include_once('../ketnoi.php');
+
+if(isset($_POST['btnedit'])){
+
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+if ($uploadOk == 0) {
+    $mes = "Your file was not upload.";
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        $mes = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        $mes = "Can't upload";
+    }
+}
+
+ $ma=$_POST['idcat'];
+ $name=$_POST['txtname'];
+$content=$_POST['txtcontent'];
+$images=$_FILES['fileToUpload']['name'];
+if($images == ""){
+	echo $images = $_POST['oldimg'];
+}
+
+$sql="UPDATE `catproducts` SET `name` = '$name',`content` = '$content',`images` = '$images' WHERE `catproducts`.`id` = $ma";
+//echo $sql;
+
+$db->exec($sql);
+
+echo "<script type='text/javascript'>alert ('".$mes."');</script>";
+
+
+
+
+}
+
+
+?>
+<script>
+	window.location.href = "catproduct.php";
+</script>
---
+++ ketnoi.php
@@ -0,0 +1,8 @@
+<?php
+
+$dsn="mysql:host=localhost;dbname=dientoandammay-inf205";
+$username="root";
+$password="";
+
+$db=new PDO($dsn,$username,$password);
+?>
